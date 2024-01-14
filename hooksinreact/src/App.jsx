edd3001f707d6f18aedf0171e695a820c3960bcf
Vivekkumar0412 <www.vivekkumar0412@gmail.com>
2024-01-14 18:45:10 +0530

    useState hook in react js :- useSate hook is used to change the state any varribel or the value in a component is basically called as state

---
+++ hooksinreact/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+function App() {
+  // let [counter, setCounter] = useState(0)
+  // let [name, setName] = useState("")
+  let [details,setDetails] = useState({counter : 0, name: ""})
+  let n = "vivek singh rajput";
+  console.log(details)
+  let v = ()=>"vivek"
+  v();
+  return (
+    
+    <>
+      <input type="text" onChange={(e)=>setDetails((prev)=>({...prev, name : e.target.value}))} />
+      <h1>{details.name} clicked the count is : {details.counter}</h1>
+      <button onClick={()=> setDetails((prev)=>({...prev, counter : details.counter + 1 }))}>INC</button>
+    </>
+  )
+}
+
+export default App
