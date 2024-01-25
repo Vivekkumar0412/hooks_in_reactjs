e5d6b31330d37a1ff70dc158c1ef23762ea8b808
Vivekkumar0412 <www.vivekkumar0412@gmail.com>
2024-01-25 23:48:20 +0530

    userefhook

--- userefhook/src/App.jsx
+++ userefhook/src/App.jsx
@@ -8,18 +8,24 @@ function App() {
   //   setCount(count + 1);
   // },[name])
   let count = useRef(0);
+  console.log(count)
   useEffect(()=>{
     count.current = count.current + 1;
   })
 
-  
+  let inputEle = useRef();
   
   return(
     <>
-    <textarea name="" id="" cols="30" rows="10"></textarea>
-      <input type="text"  onChange={(e)=> setName(e.target.value)} />
+    
+      <input type="text" ref={inputEle} onChange={(e)=> setName(e.target.value)} />
       <h1>name : {name}</h1>
       <h1>Cureent count is : {count.current}</h1>
+      <button  onClick={()=>{
+        inputEle.current.style.width = "400px";
+        inputEle.current.style.padding = "40px";
+        inputEle.current.focus();
+      }}>CLICK</button>
     </>
   )
 }
