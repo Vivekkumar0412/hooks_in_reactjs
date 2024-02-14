d44d15815ab2da4f8393c02994bb82e468d99ac4
Vivekkumar0412 <www.vivekkumar0412@gmail.com>
2024-02-14 23:59:49 +0530

    ahi

---
+++ customhook/src/App.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+// function App() {
+//   const [users, setUsers] = useState([]);
+//   useEffect(()=>{
+//     fetch("https://randomuser.me/api/")
+//     .then((res)=>res.json())
+//     .then((data)=> setUsers(data))
+//   },[])
+  
+//   // console.log(users.results[0].name.first)
+//   // console.log(users)
+//   return(
+//     <>    
+//     {users.map((elem)=>{
+//       <h1>{elem.results}</h1>
+//     })}
+//     </>
+
+//   )
+// }
+
+// export default App
+export default function App(){
+  let [user, setUser] = useState();
+  useEffect(()=>{
+    fetch("https://randomuser.me/api/")
+    .then((res)=> res.json())
+    .then((data)=> setUser(data))
+  },[]);
+  setTimeout(()=>{
+    console.log(user)
+    console.log(user.results);
+    console.log(user.results[0]);
+    console.log(user.results[0].name);
+    console.log(user.results[0].name.first);
+    console.log(typeof(user))
+  
+  })
+  return(
+    
+    <>
+     { setTimeout(()=>{
+        let nam = user.results[0].name;
+         return(nam) 
+      })}
+
+      <h1>{nam}</h1>
+      </>
+  )
+}
